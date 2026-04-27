569944f96a246f274b07cbed17c71bfb227f13b6
Rohan Awhad <rawhad@redhat.com>
2026-04-27 15:26:50 -0400

    exp_111: best config + max-num-batched-tokens=16384

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ SERVER_DEPLOYMENT_CONFIG="--data-parallel-size 2 \
   --kv-cache-dtype fp8 \
   --quantization fp8 \
   --max-num-seqs 256 \
+  --max-num-batched-tokens 16384 \
   --performance-mode interactivity \
   --disable-log-stats \
   -O3"
